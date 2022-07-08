91d7b16c4a8a2eba0db1d40737ba5c1af5de28f1
Htet Wai Yan <htetwaiyan.gaegae@gmail.com>
2022-07-08 19:54:48 +0630

    project added

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Button Got Clicked"),
+            TextButton(onPressed: () {
+              print("Text Button got clicked!!!");
+            }, child: const Text("Text Button")),
+            ElevatedButton(onPressed: (){
+              print("Elevated Button got clicked!!!");
+            }, child: const Text("Elevated Button")),
+           OutlinedButton(onPressed: (){
+              print("Outline Button got clicked!!!");
+            }, child: const Text("Outline Button")),
+            GestureDetector(
+              onTap: () { 
+                print("Image got clicked!");
+              },
+              child: Image.network(width: 50, height: 50,'https://avatars2.githubusercontent.com/u/35045612?s=280&v=4')),
+              InkWell(
+              onTap: () { 
+                print("Image got clicked with Inkwell Widget!");
+              },
+              child: Image.network(width: 50, height: 50, 'https://avatars2.githubusercontent.com/u/35045612?s=280&v=4')),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Floating Action Button got clicked!!!");
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
